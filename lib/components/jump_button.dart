d21a06379bdb32ee1c499444b95bdaedfb03f8ea
injae <injaesong0@gmail.com>
2024-06-17 04:05:46 +0900

    feat: add jump button

---
+++ lib/components/jump_button.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:princess_advanture/pink_adventure.dart';
+
+class JumpButton extends SpriteComponent with HasGameRef<PinkAdventure>, TapCallbacks {
+
+  final margin = 32;
+  final buttonSize = 64;
+
+  @override
+  FutureOr<void> onLoad() {
+    sprite  = Sprite(game.images.fromCache('HUD/Jump.png'));
+    position = Vector2(
+      game.size.x - margin - buttonSize,
+      game.size.y - margin - buttonSize,
+    );
+
+    return super.onLoad();
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    game.player.hasJumped = true;
+
+    super.onTapDown(event);
+  }
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    game.player.hasJumped = false;
+
+    super.onTapUp(event);
+  }
+
+}
