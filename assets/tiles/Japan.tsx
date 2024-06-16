be5de51138335f812de9d75af525d7cf3e7cc795
injae <injaesong0@gmail.com>
2024-06-17 04:04:24 +0900

    feat: add tiles

---
+++ assets/tiles/Japan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Japan" tilewidth="16" tileheight="16" tilecount="1320" columns="55">
+ <image source="../images/Background/Japan.png" width="892" height="392"/>
+</tileset>
---
+++ assets/tiles/Jeju.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Jeju" tilewidth="16" tileheight="16" tilecount="1320" columns="55">
+ <image source="../images/Background/Jeju.png" width="892" height="392"/>
+</tileset>
